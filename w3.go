d94cd03ee341b8e0eb82fd98952539cf6f8a8af6
victor ejike <victorejiki@gmail.com>
2026-04-15 12:13:53 +0100

    go type of outputprint

--- w3.go
+++ w3.go
@@ -1,12 +1,33 @@
+// package main 
+
+// import(
+// 	"fmt"
+// )
+
+// func main(){
+
+// 	var i, j = 19, "helo"
+// 	fmt.Print(i ,"\n")
+// 	fmt.Print(j, "\n")
+// }
+
 package main 
 
-import(
+import (
 	"fmt"
 )
 
 func main(){
 
-	var i, j = 19, "helo"
-	fmt.Print(i ,"\n")
-	fmt.Print(j, "\n")
+	var i = 15.5
+	var text = "hello world!"
+
+	fmt.Printf("%v\n", i)
+	fmt.Printf("%#v\n", i)
+	fmt.Printf("%v%%\n", i)
+	fmt.Printf("%T\n", i)
+
+	fmt.Printf("%v\n", text)
+	fmt.Printf("%#v\n", text)
+	fmt.Printf("%v%%\n", text)
 }
