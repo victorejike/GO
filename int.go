7681665bd2551c8c3b1b800d8efc88d852117d58
victor ejike <victorejiki@gmail.com>
2026-04-18 09:46:56 +0100

    interger

---
+++ int.go
@@ -0,0 +1,13 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main(){
+	var a int = 900
+	var b int = -50
+
+	fmt.Printf("type : %T value: %v\n", a, a)
+	fmt.Printf("type : %T value: %v\n", b, b)
+}
