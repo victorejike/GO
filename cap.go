99e1cea74abf054c11095c7458085c9f9d5b1428
victor ejike <victorejiki@gmail.com>
2026-04-19 11:46:21 +0100

    text editor

---
+++ cap.go
@@ -0,0 +1,24 @@
+package main
+
+import(
+	"fmt"
+)
+
+func capitalized(text string)string{
+	result := strings.ToUpper(test[0:1]) + strings.ToLower(test[1:])
+	return result
+}
+
+func transforma(text string)string{
+  tokens := strings.Fields(text)
+  var result []string
+
+  for i := 0; i < len(tokens); i++ {
+	 tokens := tokens[i]
+
+	 if tokens == "(up)" && len(result) > 0 {
+		
+	 }
+  }
+
+}
