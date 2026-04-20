aac60408c2ae28352e27297b62f6c7fc6b12f7f3
victor ejike <victorejiki@gmail.com>
2026-04-20 19:55:10 +0100

    add strings.fields

--- cap.go
+++ cap.go
@@ -2,39 +2,26 @@ package main
 
 import (
 	"fmt"
+
 	"strings"
 )
 
-func capitalized(text string) string {
+func capitalized(text string) string{
 	result := strings.ToUpper(text[0:1]) + strings.ToLower(text[1:])
 	return result
 }
+func transforma(text string)string {
+	tokens := strings.Fields(text)
+	 var  result []string
 
-func transforma(text string) string {
-	token := strings.Fields(text)
-	var result []string
+	 for i := 0; i < len(tokens); i++{
+		tokens := tokens[i]
 
-	for i := 0; i < len(result); i++ {
-		token := token[i]
-
-		if token == "(cap)" && len(result) > 0 {
+		if tokens == "(cap)" && len(result) > 0 {
 			result[len(result)-1] = capitalized(result[len(result)-1])
 			continue
 		}
 
-		if token == "(up)" && len(result) > 0 {
-			result[len(result)-1] = strings.ToUpper(result[len(result)-1])
-			continue
-		}
-
-		if token == "(low)" && len(result) > 0 {
-			result[len(result)-1] = strings.ToLower(result[len(result)-1])
-			continue
-		}
-	}
-	return result[len(text)]
-}
-
-func main() {
-	fmt.Println(transforma("hello(cap) this my first name"))
+		
+	 }
 }
